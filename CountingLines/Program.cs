7ec23e0a9369a3204b447548eed80dbbbf944a82
Cobus Smit <cobus@smitsa.co.za>
2021-03-12 15:31:35 +0200

    started supporting command line arguments to count either files or directories

--- CountingLines/Program.cs
+++ CountingLines/Program.cs
@@ -7,14 +7,24 @@ namespace CountingLines
   {
     static void Main(string[] args)
     {
-
       var system = new FileSystemAccess();
       var counter = new DirectoryLineCounter(
         system,
         new FileLineCounter(system, new LineCounter())
       );
 
-      var lines = counter.Count(@"E:\Chillisoft\DeliberatePractice\game-of-life\GameOfLife");
+      var lines = 0;
+
+      if (args.Length >= 2 && args[0] == "-f")
+      {
+        //count a file
+      }
+
+      if (args.Length >= 2 && args[0] == "-d")
+      {
+        lines = counter.Count(args[1]);
+      }
+
       Console.WriteLine($"Number of lines in file = {lines}");
 
     }
